fd79f44b211d9b4daba5999bd3c43c68023d6ca8
VladislavVolkovS <105912739+VladislavVolkovS@users.noreply.github.com>
2023-05-20 23:26:33 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,56 @@
-package com.company;
-
-import javax.swing.*;
-import java.util.Random;
-import java.util.TreeMap;
+import java.util.Scanner;
 
 public class Main {
-    static final int countFloor = 5;
-    static final int countLift = 2;
+    static int countFloor = 15;
+    static int countOfElevators = 2;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("По дефолту выбрано здание с двумя лифтами и 15 этажами, хотите выбрать другое здание?");
+        System.out.println("1 — Да");
+        System.out.println("2 — Нет");
+        int choice = scanner.nextInt();
+        if (choice == 1){
+            System.out.println("Введите количество этажей:");
+            countFloor = scanner.nextInt();
+            while (countFloor < 1){
+                System.out.println("Количество этажей не может быть меньше 0! Введите корректное количество:");
+                countFloor = scanner.nextInt();
+            }
+            System.out.println("Введите количество лифтов:");
+            countOfElevators = scanner.nextInt();
+            while (countOfElevators < 1){
+                System.out.println("Количество лифтов не может быть меньше 0! Введите корректное количество:");
+                countOfElevators = scanner.nextInt();
+            }
+        }
 
+        System.out.println("Введите количество заявок:");
+        int requestsNumber = scanner.nextInt();
+        while (requestsNumber < 1){
+            System.out.println("Количество заявок должно быть больше 0! Введите корректное количество:");
+            requestsNumber = scanner.nextInt();
+        }
+        System.out.println("Введите интервал генерации в мс:");
+        int interval = scanner.nextInt();
+        while (interval < 1){
+            System.out.println("Интервал должен быть больше 0! Введите корректное число:");
+            interval = scanner.nextInt();
+        }
         Controller controller = new Controller();
 
-        Thread lifts = new Thread(new Work(controller));
-
-        Visitor aaa = new Visitor();
+        Thread lifts = new Thread(new WorkSystem(controller));
         lifts.start();
 
-        for (int i = 0; i < 100; i++) {
-            Visitor visitor = new Visitor();
-            visitor.callLift(controller);
-            //System.out.println(i+" "+visitor.fromFloor+" "+visitor.toFloor);
-
+        for (int i = 0; i < requestsNumber; i++) {
+            Passenger passenger = new Passenger();
+            passenger.callLift(controller);
             try {
-                Thread.sleep(2000);
-                //System.out.println("added person");
+                Thread.sleep(interval);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
-        int a = 0;
     }
 
 }
